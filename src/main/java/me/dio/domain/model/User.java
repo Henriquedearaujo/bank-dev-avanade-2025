a9a899323c618cbdb07b0176c95fb91bdca0f5fd
Henriquedearaujo <ricke_cris@hotmail.com>
2025-02-22 16:26:27 -0300

    classes criadas

--- src/main/java/me/dio/domain/model/User.java
+++ src/main/java/me/dio/domain/model/User.java
@@ -2,16 +2,10 @@ package me.dio.domain.model;
 
 import java.util.List;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 
-@Entity(name = "tb_user")
+@Entity
+@Table(name = "tab_user")
 public class User {
 
     @Id
