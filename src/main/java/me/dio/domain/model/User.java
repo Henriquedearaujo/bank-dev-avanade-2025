5628d83d7f4cefaa5c7c6e17c81b0cbe7291f844
Henriquedearaujo <ricke_cris@hotmail.com>
2025-02-22 16:41:20 -0300

    Revert "classes criadas"
    This reverts commit a9a899323c618cbdb07b0176c95fb91bdca0f5fd.

--- src/main/java/me/dio/domain/model/User.java
+++ src/main/java/me/dio/domain/model/User.java
@@ -2,10 +2,16 @@ package me.dio.domain.model;
 
 import java.util.List;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 
-@Entity
-@Table(name = "tab_user")
+@Entity(name = "tb_user")
 public class User {
 
     @Id
